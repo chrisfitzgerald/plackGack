60e1c1213678ec1f31cc2bee3539dd10c6b74906
Chris Fitzgerald <cfitzgerald8888@gmail.com>
2025-06-22 00:26:01 -0400

    relax

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -23,6 +23,14 @@ const eslintConfig = [
     ]
   },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["src/**/*.ts", "src/**/*.tsx"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "prefer-const": "off",
+      "@typescript-eslint/no-unused-vars": "off"
+    }
+  }
 ];
 
 export default eslintConfig;
